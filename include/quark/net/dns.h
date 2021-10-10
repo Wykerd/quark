d28958fad224eb6666a0a37fe38f0f99637d8f87
Daniel Wykerd <danielwykerd@gmail.com>
2021-10-10 23:49:21 +0200

    feat(dns): basic dns implementation

---
+++ include/quark/net/dns.h
@@ -0,0 +1,16 @@
+#ifndef QRK_NET_DNS_H
+#define QRK_NET_DNS_H
+
+#include <uv.h>
+#include <quark/std/stream.h>
+
+typedef void (*qrk_getaddrinfo_cb)(qrk_loop_t *loop, qrk_err_t *err, struct sockaddr *addr);
+
+int qrk_dns_is_numeric_host_af (const char *hostname, int family);
+int qrk_dns_is_numeric_host_v6 (const char *hostname);
+int qrk_dns_is_numeric_host_v4 (const char *hostname);
+int qrk_dns_is_numeric_host_v (const char *hostname);
+int qrk_dns_is_numeric_host (const char *hostname);
+void qrk_dns_getaddrinfo (qrk_loop_t *loop, const char *hostname, const char *port, qrk_getaddrinfo_cb callback);
+
+#endif
--- src/net/dns.c
+++ src/net/dns.c
@@ -1,1 +1,101 @@
 #include <quark/net/dns.h>
+#include <netdb.h>
+#include <string.h>
+
+int qrk_dns_is_numeric_host_af (const char *hostname, int family) 
+{
+	struct in6_addr dst;
+	return uv_inet_pton(family, hostname, &dst) == 0;
+}
+
+int qrk_dns_is_numeric_host_v6 (const char *hostname) 
+{
+	return qrk_dns_is_numeric_host_af(hostname, AF_INET6);
+}
+
+int qrk_dns_is_numeric_host_v4 (const char *hostname) 
+{
+	return qrk_dns_is_numeric_host_af(hostname, AF_INET);
+}
+
+int qrk_dns_is_numeric_host_v (const char *hostname) 
+{
+	int v = 0;
+	if (qrk_dns_is_numeric_host_v4(hostname))
+		v = 4;
+	else if (qrk_dns_is_numeric_host_v6(hostname))
+		v = 6;
+	return v;
+}
+
+int qrk_dns_is_numeric_host (const char *hostname) 
+{
+	return qrk_dns_is_numeric_host_v6(hostname) ||
+		   qrk_dns_is_numeric_host_v4(hostname);
+}
+
+typedef struct qrk_getaddrinfo_s {
+	qrk_loop_t *loop;
+	qrk_getaddrinfo_cb callback;
+	uv_getaddrinfo_t req;
+} qrk_getaddrinfo_t;
+
+static void qrk__dns_getaddrinfo_cb (uv_getaddrinfo_t* req, int status, struct addrinfo* res)
+{
+	qrk_getaddrinfo_t *ctx = (qrk_getaddrinfo_t *)req->data;
+	if (status != 0)
+	{
+		qrk_err_t err = {
+				.code = status,
+				.origin = QRK_EO_UV,
+				.target_type = ctx->loop->type,
+				.target = ctx->loop
+		};
+		ctx->callback(ctx->loop, &err, NULL);
+		goto cleanup;
+	}
+	ctx->callback(ctx->loop, NULL, res->ai_addr);
+cleanup:
+	uv_freeaddrinfo(res);
+	qrk_free(ctx->loop->m_ctx, ctx);
+}
+
+void qrk_dns_getaddrinfo (qrk_loop_t *loop, const char *hostname, const char *port, qrk_getaddrinfo_cb callback)
+{
+	qrk_getaddrinfo_t *ctx = qrk_malloc(loop->m_ctx, sizeof(qrk_getaddrinfo_t));
+	if (ctx == NULL)
+	{
+		qrk_err_t err = {
+			.code = QRK_E_OOM,
+			.origin = QRK_EO_IMPL,
+			.target_type = loop->type,
+			.target = loop
+		};
+		callback(loop, &err, NULL);
+	}
+	ctx->req.data = ctx;
+
+	struct addrinfo hints;
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+
+	int r = uv_getaddrinfo(
+		ctx->loop->loop,
+		&ctx->req,
+		qrk__dns_getaddrinfo_cb,
+		hostname, port, &hints
+	);
+
+	if (r != 0)
+	{
+		qrk_err_t err = {
+			.code = r,
+			.origin = QRK_EO_UV,
+			.target_type = loop->type,
+			.target = loop
+		};
+		callback(loop, &err, NULL);
+	}
+}
